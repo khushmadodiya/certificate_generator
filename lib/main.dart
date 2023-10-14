1b1d5050f494ebbbfea63de8b0a8efffa8020545
khush <khushvantmadodiya@gmail.com>
2023-10-14 17:24:45 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'dart:typed_data';
+
+import 'package:certificate_generator/firstScreen.dart';
+import 'package:certificate_generator/pdf.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:UserName(),
+    );
+  }
+}
